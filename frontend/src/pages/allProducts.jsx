19e8b4a9649c47b406c64f892524755a8882c70e
Abderrahim <dicou940@gmail.com>
2025-09-01 09:40:50 +0100

    git add all products page.

---
+++ frontend/src/pages/allProducts.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+import { dummyProducts } from "../assets/assets"
+import ProductsCard from "../components/productsCard"
+
+export default function AllProducts() {
+    return (
+        <div className="flex flex-col gap-8" >
+            <div className="items-end flex flex-col w-max" >
+                <h1 className="text-2xl font-semibold uppercase" >all products</h1>
+                <div className="w-16 h-0.5 bg-primary rounded-full" ></div>
+            </div>
+            <div className="flex flex-wrap gap-6" >
+                {dummyProducts.map(product => (
+                    <Link to={`/products/${product.category}`} key={product._id}  >
+                        <ProductsCard product={product} />
+                    </Link>
+                ))}
+            </div>
+        </div>
+    )
+}
