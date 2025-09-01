6077ec55490c318b34513b48229baa9d5c31adde
Abderrahim <dicou940@gmail.com>
2025-09-01 17:52:04 +0100

    add product page

---
+++ frontend/src/pages/product.jsx
@@ -0,0 +1,75 @@
+import { useParams } from "react-router-dom"
+
+import { dummyProducts , assets } from "../assets/assets"
+import { useState } from "react"
+
+export default function Product() {
+    const { id } = useParams()
+    const product = dummyProducts.find(ele => ele._id === id)
+
+    const [thumbnail, setThumbnail] = useState(product.image[0])
+    console.log(product)
+    return (
+        <div className="max-w-6xl w-full px-6">
+            <p>
+                <span>Home</span> /
+                <span> Products</span> /
+                <span> {product.category}</span> /
+                <span className="text-primary"> {product.name}</span>
+            </p>
+
+            <div className="flex flex-col md:flex-row gap-16 mt-4">
+                <div className="flex gap-3">
+                    <div className="flex flex-col gap-3">
+                        {product.image.map((image, index) => (
+                            <div key={index} onClick={() => setThumbnail(image)} className="border max-w-24 border-gray-500/30 rounded overflow-hidden cursor-pointer" >
+                                <img src={image} alt={`Thumbnail ${index + 1}`} />
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className="border border-gray-500/30 max-w-100 rounded overflow-hidden">
+                        <img src={thumbnail} alt="Selected product" className="w-full h-full object-cover" />
+                    </div>
+                </div>
+
+                <div className="text-sm w-full md:w-1/2">
+                    <h1 className="text-3xl font-medium">{product.name}</h1>
+
+                    <div className="flex items-center gap-0.5 mt-1">
+                        {Array(5).fill("").map((_, i) => (
+                            <img
+                                key={i}
+                                src={i < 4 ? assets.star_icon : assets.star_dull_icon}
+                                className="w-3 md:w-3.5"
+                            />
+                        ))}
+                        <p className="text-base ml-2">(4)</p>
+                    </div>
+
+                    <div className="mt-6">
+                        <p className="text-gray-500/70 line-through">MRP: ${product.price}</p>
+                        <p className="text-2xl font-medium">MRP: ${product.offerPrice}</p>
+                        <span className="text-gray-500/70">(inclusive of all taxes)</span>
+                    </div>
+
+                    <p className="text-base font-medium mt-6">About Product</p>
+                    <ul className="list-disc ml-4 text-gray-500/70">
+                        {product.description.map((desc, index) => (
+                            <li key={index}>{desc}</li>
+                        ))}
+                    </ul>
+
+                    <div className="flex items-center mt-10 gap-4 text-base">
+                        <button className="w-full py-3.5 cursor-pointer font-medium bg-gray-100 text-gray-800/80 hover:bg-gray-200 transition" >
+                            Add to Cart
+                        </button>
+                        <button className="w-full py-3.5 cursor-pointer font-medium bg-primary text-white hover:bg-primary-dull transition" >
+                            Buy now
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
